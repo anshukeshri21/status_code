a255b4a6df53f04f93dbf2815a27baf6f63a922d
wreckx-in-scene <amoghdandotiya123@gmail.com>
2024-08-25 07:18:42 +0530

    Complete website added

--- website_code/src/App.js
+++ website_code/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Details from './Components/DetailsSection';
 import VideoComponent from './Components/Animated Vid';
 import ImageComponent from './Components/Image_component';
+import Hardware from './Components/HardwareDetails';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='/details' element = {<Details/>}/>
           <Route path='/animated-video' element = {<VideoComponent/>}/>
           <Route path='/integrated-circuit' element = {<ImageComponent/>}/>
+          <Route path='/hardware-details' element = {<Hardware/>}/>
         </Routes>
       </Router>
     </div>
---
+++ website_code/src/Components/HardwareDetails/HardwareComponent.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HardwareContainer = styled.div`
+  margin: 20px;
+  padding: 50px;
+  color: white;
+  background-color: #0d0d0c;
+  border-radius: 20px;
+`
